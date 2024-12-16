9122d4b48706b8ffea83b89a841a8caf9043ae40
femelo22 <luiz123jfmg@gmail.com>
2024-12-16 20:29:54 -0300

    feat: iniciando projeto e configs

---
+++ src/main/java/br/com/lfmelo/EmailSenderMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.lfmelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSenderMsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/lfmelo/entities/Email.java
@@ -0,0 +1,4 @@
+package br.com.lfmelo.entities;
+
+public record Email(String to, String subject, String body) {
+}
